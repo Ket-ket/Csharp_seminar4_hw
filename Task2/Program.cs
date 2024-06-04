0caacc4f126971b8950d5d400289212dd9f96948
Ekaterina Efimova <NiKaT2009@yandex.ru>
2024-06-04 14:42:41 +0300

    Добавлено решение 3-х задач

---
+++ Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+// Например:
+// [344 452 341 125] => 2
+
+
+int ReadInt(string text){
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreatedArr(int size, int leftRange, int rightRange){
+    int[] Array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = rand.Next(leftRange, rightRange);
+    }
+    return Array;
+}
+
+void PrintArr(int[] array){
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int EvenNum(int[] array){
+    int count = 0;
+    foreach(int num in array){
+        if(num % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int size = ReadInt("Введите размер массива: ");
+int[] arr = CreatedArr(size, 100, 1000);
+PrintArr(arr);
+int countEvenNum = EvenNum(arr);
+Console.WriteLine($"Количество четных чисел в массиве: {countEvenNum}");
---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+// Например:
+// [1 3 5 6 7 8] => [8 7 6 5 3 1]
+
+using System.IO.Compression;
+
+int ReadInt(string text){
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreatedArr(int size, int leftRange, int rightRenge){
+    int[] arr = new int[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < size; i++){
+        arr[i] = rand.Next(leftRange, rightRenge);
+    }
+    return arr;
+}
+
+void PrintArr(int[] array){
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int[] ChangeArr(int[] arr){
+    int[] array = new int[arr.Length];
+    for(int i = 0; i < arr.Length; i++){
+        array[i] = arr[arr.Length - 1 -i]; 
+    }
+    return array;
+}
+
+int size = ReadInt("Введите длину массива: ");
+int[] newArray = CreatedArr(size, 0, 10);
+PrintArr(newArray);
+int[] finalArr = ChangeArr(newArray);
+PrintArr(finalArr);
